e50126c34618d9cadd1feec5635f2444d021798e
Grahame Bowland <grahame@oreamnos.com.au>
2020-12-01 22:38:40 +0800

    part 1 passing

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,27 @@
+use std::env;
+use std::fs::File;
+use std::io::BufRead;
+use std::collections::HashMap;
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+
+    let file = File::open(filename).unwrap();
+    let mut map = HashMap::new();
+    for line in std::io::BufReader::new(file).lines() {
+        let val: i32 = line.unwrap().parse::<i32>().unwrap();
+        map.insert(val, true);
+    }
+    for i in 1..2020 {
+        if !map.contains_key(&i) {
+            continue;
+        }
+        let j = 2020 - i;
+        if map.contains_key(&j) {
+            println!("{}, {}, {}", i, j, i * j);
+        }
+    }
 }
