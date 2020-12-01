82f45dbad6e37fed137bdace14f7d72372c12bee
Grahame Bowland <grahame@oreamnos.com.au>
2020-12-01 22:52:45 +0800

    day1 part2 complete

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -4,17 +4,20 @@ use std::io::BufRead;
 use std::collections::HashMap;
 
 
-fn sum_to(map: &HashMap<i32, bool>, target: i32) -> (i32, i32) {
+fn sum_to(map: &HashMap<i32, bool>, target: i32) -> Option<(i32, i32)> {
     for i in 1..target {
         if !map.contains_key(&i) {
             continue;
         }
         let j = target - i;
+        if j == i {
+            continue;
+        }
         if map.contains_key(&j) {
-            return (i, j);
+            return Some((i, j));
         }
     }
-    return (-1, -1);
+    return None;
 }
 
 
@@ -29,6 +32,21 @@ fn main() {
         map.insert(val, true);
     }
 
-    let (a, b) = sum_to(&map, 2020);
+    let (a, b) = sum_to(&map, 2020).unwrap();
     println!("a={}, b={}, a*b={}", a, b, a * b);
+
+    for i in 1..2020 {
+        if !map.contains_key(&i) {
+            continue;
+        }
+        let j = 2020 - i;
+        match sum_to(&map, j) {
+            Some((a, b)) => {
+                if (a != i) && (b != i) && (a != b) {
+                    println!("a={}, b={}, i={}, a*b*i={}", a, b, i, a * b * i);
+                }
+            }
+            None => {}
+        }
+    }
 }
